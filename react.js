e6df3309412721e1905035f9144eb08b7314ba44
Debasish Rana <debasishrana2002@gmail.com>
2024-02-13 10:05:32 +0530

    react code

---
+++ react.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = ()=>{
+          return(
+                    <h1>my name is debasish rana</h1>
+          )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
