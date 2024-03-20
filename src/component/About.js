7acaaa6ee3bde577a4a30e05d0b5edf66e33ba70
Debashish Rana <152690128+Debasish-rana@users.noreply.github.com>
2024-03-20 18:52:06 +0530

    #feth github user deta

--- src/component/About.js
+++ src/component/About.js
@@ -5,10 +5,8 @@ const About = () => {
     <div>
      
       <h1>This is my about us page</h1>
-      <Fnuser name={"Debasish Rana"} location={"Palaschabri"} component={"This is a functional component"} />
       <UserClass name={"Debasish Rana"} location={"Palaschabri"} component={"This is a class component"}/>
-       
-    </div>
+       </div>
   );
 };
 export default About;
--- src/component/UserClass.js
+++ src/component/UserClass.js
@@ -4,24 +4,32 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-    };
+      userInfo:{
+        
+      }
+    }
+    
   }
-
+ async componentDidMount(){
+  const data = await fetch("https://api.github.com/users/Debasish-rana")
+  const json = await data.json();
+  console.log(json)
+  this.setState({
+    userInfo:json,
+  })
+}
   render() {
-    const { name, location, component } = this.props;
-    const { count }= this.state
+    const { name, location, avatar_url, url } = this.state.userInfo;
+    if (this.state === 0) {
+      return <ShimerUi />;
+    }
     return (
       <div className="class-card">
+      <img src= {avatar_url} />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h3>Components: {component}</h3>
-        <h3>count = {count}</h3>
-        <button onClick={()=>{
-          this.setState({
-            count:this.state.count + 1
-          })
-        }}>countincrease</button>
+        <h3>Github: {url}</h3>
+        
       </div>
     );
   }
